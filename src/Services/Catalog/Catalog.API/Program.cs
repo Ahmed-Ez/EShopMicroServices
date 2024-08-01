10b22b843f7bf968015fecd6df102e87a485844d
ahmedezz <ahmedezzcs@gmail.com>
2024-08-01 21:19:39 +0300

    Setup custom query and command handlers, Create first endpoint, setup Carter and Mapster

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add Services
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+});
 
 var app = builder.Build();
 
 // Configure HTTP Request Pipeline
+app.MapCarter();
 
 app.Run();
