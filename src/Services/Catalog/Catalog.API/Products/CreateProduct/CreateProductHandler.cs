6004e66a2eae9efddb8343fdfa102c40fc1387f9
ahmedezz <ahmedezzcs@gmail.com>
2024-08-04 18:09:18 +0300

    BuildingBlocks: Add FluentValidation

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,16 +1,36 @@
 ﻿
 
+
+
 namespace Catalog.API.Products.CreateProduct
 {
     public record CreateProductCommand(string Name,List<string> Category,string Description,string ImageFile,decimal Price)
         :ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
 
-    internal class CreateProductCommandHandler(IDocumentSession session)
+    public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductCommandValidator()
+        {
+            RuleFor(p => p.Name).NotEmpty().WithMessage("Name is required");
+            RuleFor(p => p.Category).NotEmpty().WithMessage("Category is required");
+            RuleFor(p => p.ImageFile).NotEmpty().WithMessage("ImageFile is required");
+            RuleFor(p => p.Price).GreaterThan(0).WithMessage("Price must be greater than 0");
+        }
+    }
+
+    internal class CreateProductCommandHandler(IDocumentSession session,IValidator<CreateProductCommand> validator)
         : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
+            var result = await validator.ValidateAsync(command,cancellationToken);
+            var errors = result.Errors.Select(x => x.ErrorMessage).ToList();
+            if (errors.Any())
+            {
+                throw new ValidationException(errors.FirstOrDefault());
+            }
+
             var product = new Product
             {
                 Name = command.Name,
