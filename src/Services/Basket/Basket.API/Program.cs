e812aa388d2aa8fce68351e6c17eee9075bea50a
ahmedezz <ahmedezzcs@gmail.com>
2024-08-05 23:15:44 +0300

    Basket.API: Repository pattern and marten setup

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -10,6 +10,12 @@ builder.Services.AddMediatR(config =>
     config.AddOpenBehavior(typeof(LogginBehavior<,>));
 });
 
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+    opts.Schema.For<ShoppingCart>().Identity(x => x.UserName);
+}).UseLightweightSessions();
+
 builder.Services.AddCarter();
 
 var app = builder.Build();
