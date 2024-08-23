ee7a4ad3624f221e15695f14389408e457c105d3
ahmedezz <ahmedezzcs@gmail.com>
2024-08-23 16:48:53 +0300

    Odering.API: Finalize endpoints

--- src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderCommand.cs
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderCommand.cs
@@ -1,17 +1,16 @@
-﻿namespace Ordering.Application.Orders.Commands.NewFolder
+﻿namespace Ordering.Application.Orders.Commands.CreateOrder;
+
+public record CreateOrderCommand(OrderDto Order)
+: ICommand<CreateOrderResult>;
+
+public record CreateOrderResult(Guid Id);
+
+public class CreateOrderCommandValidator : AbstractValidator<CreateOrderCommand>
 {
-    public record CreateOrderCommand(OrderDto Order)
-    : ICommand<CreateOrderResult>;
-
-    public record CreateOrderResult(Guid Id);
-
-    public class CreateOrderCommandValidator : AbstractValidator<CreateOrderCommand>
+    public CreateOrderCommandValidator()
     {
-        public CreateOrderCommandValidator()
-        {
-            RuleFor(x => x.Order.OrderName).NotEmpty().WithMessage("Name is required");
-            RuleFor(x => x.Order.CustomerId).NotNull().WithMessage("CustomerId is required");
-            RuleFor(x => x.Order.OrderItems).NotEmpty().WithMessage("OrderItems should not be empty");
-        }
+        RuleFor(x => x.Order.OrderName).NotEmpty().WithMessage("Name is required");
+        RuleFor(x => x.Order.CustomerId).NotNull().WithMessage("CustomerId is required");
+        RuleFor(x => x.Order.OrderItems).NotEmpty().WithMessage("OrderItems should not be empty");
     }
 }
