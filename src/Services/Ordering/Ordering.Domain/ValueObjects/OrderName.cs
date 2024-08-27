bc9d2e68f38fa43517677fff0c3dc182b3b6fc06
ahmedezz <ahmedezzcs@gmail.com>
2024-08-27 16:00:50 +0300

    add rabbitmq to docker compose

--- src/Services/Ordering/Ordering.Domain/ValueObjects/OrderName.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/OrderName.cs
@@ -3,7 +3,7 @@ namespace Ordering.Domain.ValueObjects
 {
     public record OrderName
     {
-        private const int DefaultLength = 5;
+        private const int DefaultLength = 3;
         public string Value { get; }
         private OrderName(string value) => Value = value;
         public static OrderName Of(string value)
