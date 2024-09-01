d4a4e96f2839ab1c86acba16cb0c55d7fcfa335f
ahmedezz <ahmedezzcs@gmail.com>
2024-09-01 20:30:28 +0300

    add yarp api gateway

--- src/Services/Ordering/Ordering.Domain/ValueObjects/OrderName.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/OrderName.cs
@@ -3,7 +3,7 @@ namespace Ordering.Domain.ValueObjects
 {
     public record OrderName
     {
-        private const int DefaultLength = 3;
+        private const int DefaultLength = 5;
         public string Value { get; }
         private OrderName(string value) => Value = value;
         public static OrderName Of(string value)
